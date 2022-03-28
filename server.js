3a5a0665cce87f2d7ce17d2abbf94758759d2098
João Matos <joao.matos@construtorapatriani.com.br>
2022-03-28 17:18:33 -0300

    path folders ok

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const res = require('express/lib/response')
 const app = express()
 
 
+app.use('/static', express.static('public'));
+
 app.get("/wallet", function(req, res) {
     res.sendFile(__dirname + '/index.html')   
 })
 
 
-app.listen(3500, () => console.log('server is running'))
+app.listen(3000, () => console.log('server is running'))
